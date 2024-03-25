f7d0d7cee4edf3d7fcad78f6c5492f38cf74b1a8
Eunsu Song <eng-sthd@naver.com>
2024-03-25 18:21:06 +0900

    practice about communityBoard

---
+++ memberProject/src/main/java/com/javateam/memberProject/dao/FileDAO.java
@@ -0,0 +1,15 @@
+package com.javateam.memberProject.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javateam.memberProject.domain.UploadFile;
+
+public interface FileDAO extends JpaRepository<UploadFile, Integer> {
+	 
+	public UploadFile findOneByFileName(String fileName);
+	
+	public UploadFile findOneById(int id);
+	
+	public void deleteById(int id);
+	
+}
